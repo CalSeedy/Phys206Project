f88dbfa1b2d45abcb72e44c1d8bf4af313b2f9d6
CalSeedy <callumseed@hotmail.co.uk>
2019-03-18 09:11:48 +0000

    nothing new...just messing around with peak detection

--- GuitarTuner/GuitarTuner/test.py
+++ GuitarTuner/GuitarTuner/test.py
@@ -1,6 +1,21 @@
 import pydub as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import referenceFreqs, math
+
+
+def getNotes(tuning):
+    all = ["A", "A#/Bb", "B", "C","C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"]
+    
+
+def getNearest(frequency: float, tuning: str):
+    notes = getNotes(tuning)
+    dif = [abs(x - frequency) for x in tuning]
+    print(dif)
+    f = min(dif)
+    ind = dif.index(f)
+    return (ind, notes[ind])
+
 
 # loading sound data
 sound = pd.AudioSegment.from_mp3("../Samples/1st_String_E_64kb.mp3")
@@ -41,5 +56,10 @@ mx = abs(freq * sr)
 print("Maximum at: %f Hz" % (mx))
 
 
+tuning = referenceFreqs.getFreqs("standard")
+nearest = getNearest(mx, tuning)
+f_nearest = tuning[nearest[0]]
 # freq to cents
-#c= 1200 * math.log2(f_max / f_nearest)
+#c = 1200 * math.log2(f_max / f_nearest)
+c = 1200 * math.log2(mx / f_nearest)
+print ("offset: %d cents (Nearest: %s)" % (c, nearest[1]) )
